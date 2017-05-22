77ccdb22b481bdb1840586bb2efe51da049dd25e
wangjinyu <jinyu5@jiaju.com>
2017-05-22 16:45:24 +0800

    完善了Demo类

--- src/Demo.php
+++ src/Demo.php
@@ -2,18 +2,37 @@
 
 class Demo
 {
+
     public function __construct()
-    {
-    }
+    {}
 
     /**
-     * @param int $var1
-     * @param int $var2
+     *
+     * @param int $var1            
+     * @param int $var2            
      *
      * @return int
      */
     public function add($var1, $var2)
     {
         return $var1 + $var2;
+    }
+
+    public function sub($var1, $var2)
+    {
+        return $var1 - $var2;
+    }
+
+    public function mul($var1, $var2)
+    {
+        return $var1 * $var2;
+    }
+
+    public function div($var1, $var2)
+    {
+        if ($var2 === 0) {
+            throw new UnexpectedValueException('Divided by Zero');
+        }
+        return $var1 / $var2;
     }
 }
