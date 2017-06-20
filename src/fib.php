fd3285db4fc126cbaca9be9f767ba7933bd1b4cf
wangjinyu <jinyu5@jiaju.com>
2017-06-20 10:24:06 +0800

    fib.php

--- src/fib.php
+++ src/fib.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * 递归方式.
  *
@@ -14,14 +15,14 @@ function fib1($n)
     if ($n == 1) {
         return 1;
     }
-
+    
     return fib1($n - 1) + fib1($n - 2);
 }
 
 /**
  * 循环方式.
  *
- * @param number $n
+ * @param number $n            
  *
  * @return number
  */
@@ -29,13 +30,13 @@ function fib2($n)
 {
     $x = 0;
     $y = 1;
-    for ($i = 1; $i < $n; ++$i) {
+    for ($i = 1; $i < $n; ++ $i) {
         $y = $x + $y;
         $x = $y - $x;
     }
-
+    
     return $y;
 }
 
-// print fib1(30) . PHP_EOL;
-// print fib2(30).PHP_EOL;
+print fib1(30) . PHP_EOL;
+print fib2(30) . PHP_EOL;
