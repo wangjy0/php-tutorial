27c0343068cde27dd498980b0088fed91700bfcd
wangjinyu <wangjy0@163.com>
2018-04-17 14:08:59 +0800

    修改mq示例使可用

--- src/mq/send.php
+++ src/mq/send.php
@@ -1,20 +1,20 @@
 <?php
-
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../../vendor/autoload.php';
 use PhpAmqpLib\Connection\AMQPStreamConnection;
 use PhpAmqpLib\Message\AMQPMessage;
 
 $connection = new AMQPStreamConnection('localhost', 5672, 'guest', 'guest');
 $channel = $connection->channel();
 
-
 $channel->queue_declare('hello', false, false, false, false);
 
-$msg = new AMQPMessage('Hello World!');
-$channel->basic_publish($msg, '', 'hello');
-
-echo " [x] Sent 'Hello World!'\n";
-
+for ($i = 0; $i < 100; $i ++) {
+    $msg = new AMQPMessage('Hello World!' . $i);
+    $channel->basic_publish($msg, '', 'hello');
+    
+    echo " [x] Sent " . $msg->getBody() . PHP_EOL;
+    sleep(1);
+}
 $channel->close();
 $connection->close();
 
