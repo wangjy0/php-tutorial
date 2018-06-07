b011fc98241d48a997d53372f71e20cf28d117bf
wangjinyu <wangjy0@163.com>
2018-06-07 14:47:02 +0800

    2018年06月 7日 14:46:52

--- src/spl/iterator.php
+++ src/spl/iterator.php
@@ -1,33 +1,12 @@
 <?php
-/* 
+ 
 $array = range(1, 100);
 
-$b = new ArrayIterator($array);
-$b->append('china');
-$c = $b->getArrayCopy();
-var_dump($b);
-var_dump($c);
- */
-// foreach (new InfiniteIterator(new ArrayIterator($array)) as $value) {
-// print($value . PHP_EOL);
-// }
-
-// foreach (new LimitIterator(new ArrayIterator($array), 20, 10) as $value) {
-// print($value . PHP_EOL);
-// }
-
-// foreach (new DirectoryIterator(__DIR__) as $item) {
-// var_dump($item);
-// }
-
-// foreach (new FilesystemIterator(__DIR__) as $item){
-// var_dump($item);
-// }
-
-// foreach (new RecursiveDirectoryIterator(__DIR__) as $item){
-// var_dump($item);
-// }
-/* 
+// $b = new ArrayIterator($array);
+// $b->append('china');
+// $c = $b->getArrayCopy();
+// var_dump($b);
+// var_dump($c);
 $array = [
     0 => 'a',
     1 => array(
@@ -51,22 +30,40 @@ $array = [
     4 => 'c'
 ];
 
-$iterator = new RecursiveArrayIterator($array);
-$iterator = new RecursiveIteratorIterator($iterator);
-foreach ($iterator as $item){
-    var_dump($item);
-}
- */
-// foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(dirname(__DIR__))) as $item) {
+$dir = dirname(dirname(__DIR__));
+// foreach (new RecursiveIteratorIterator(new RecursiveArrayIterator($array)) as $item){
 //     var_dump($item);
 // }
 
-class FileTypeFilterIterator extends FilterIterator
+// foreach (new InfiniteIterator(new ArrayIterator($array)) as $value) {
+//     print($value . PHP_EOL);
+// }
+
+// foreach (new LimitIterator(new ArrayIterator($array), 20, 10) as $value) {
+//     print($value . PHP_EOL);
+// }
+
+// foreach (new DirectoryIterator(__DIR__) as $item) {
+//     var_dump($item);
+// }
+
+// foreach (new FilesystemIterator(__DIR__) as $item) {
+//     var_dump($item);
+// }
+
+// foreach (new DirectoryIterator(dirname(__DIR__)) as $item) {
+//     print $item->getPathname() . PHP_EOL;
+// }
+
+// foreach (new RecursiveIteratorIterator(new RecursiveDirectoryIterator(dirname(__DIR__))) as $item) {
+//     print $item->getPathname() . PHP_EOL;
+// }
+class FileTypeFilterIterator1 extends RecursiveFilterIterator
 {
 
     public $FILTERS;
 
-    public function __construct($iterator, $ext)
+    public function __construct(RecursiveIterator $iterator, $ext)
     {
         $this->FILTERS = $ext;
         parent::__construct($iterator);
@@ -74,11 +71,41 @@ class FileTypeFilterIterator extends FilterIterator
 
     public function accept()
     {
-        $item = $this->current();
-        return in_array(strtolower(pathinfo($item->getFilename(), PATHINFO_EXTENSION)), $this->FILTERS);
+        return in_array(strtolower(pathinfo($this->current()->getFilename(), PATHINFO_EXTENSION)), $this->FILTERS);
     }
 }
+$ir = new RecursiveDirectoryIterator($dir);
+$ir = new FileTypeFilterIterator1($ir , ['json']);
+$ir = new RecursiveIteratorIterator($ir);
+foreach ($ir as $item){
+   print $item->getPathname() . PHP_EOL;
+}
+class FileTypeFilterIterator2 extends FilterIterator
+{
 
-// foreach (new FileTypeFilterIterator(new RecursiveIteratorIterator(new RecursiveDirectoryIterator(dirname(__DIR__))), ['php']) as $item){
-//     var_dump($item);
-// }
+    public $FILTERS;
+
+    public function __construct(Iterator $iterator, $ext)
+    {
+        $this->FILTERS = $ext;
+        parent::__construct($iterator);
+    }
+
+    public function accept()
+    {
+        return in_array(strtolower(pathinfo($this->current()->getFilename(), PATHINFO_EXTENSION)), $this->FILTERS);
+    }
+}
+$ir = new RecursiveDirectoryIterator($dir);
+$ir = new RecursiveIteratorIterator($ir);
+$ir = new FileTypeFilterIterator2($ir , ['json']);
+foreach ($ir as $item){
+   print $item->getPathname() . PHP_EOL;
+}
+
+$ir = new RecursiveDirectoryIterator($dir);
+$ir = new ParentIterator($ir);
+$ir = new RecursiveIteratorIterator($ir, RecursiveIteratorIterator::CHILD_FIRST);
+foreach ($ir as $v){
+//     print ($v->getPathname()) . PHP_EOL;
+}
