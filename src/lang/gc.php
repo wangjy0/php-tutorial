ce716b48a03d1d65dd1607bcfa455b1f62ca446a
Wangjinyu <wangjy0@163.com>
2019-08-27 20:19:06 +0800

    更新示例

--- src/lang/gc.php
+++ src/lang/gc.php
@@ -3,7 +3,22 @@
 //$a = 'string';
 //$a = 10;
 //$a = new stdClass();
-$a = [1,2,3,4,5];
+//$a = [1,2,3,4,5];
 //$b = $a;
 
-xdebug_debug_zval('a');
+//xdebug_debug_zval('a');
+
+//php -dzend.enable_gc=0 -dmemory_limit=-1 -n gc.php
+
+class Foo
+{
+    public $var = '3.1415962654';
+}
+
+for ( $i = 0; $i <= 20000; $i++ )
+{
+    $a = new Foo;
+    $a->self = $a;
+}
+
+echo memory_get_peak_usage(), "\n";
