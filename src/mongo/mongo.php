cbb1a2d4f55a6c6b6a81afc3978b01985f1cb22a
Wangjinyu <wangjy0@163.com>
2019-09-09 20:49:49 +0800

    更新示例

--- src/mongo/mongo.php
+++ src/mongo/mongo.php
@@ -1,36 +1,18 @@
 <?php
+require_once "../../vendor/autoload.php";
 
-header('Content-type: text/html; charset=utf-8');
+$filename = 'https://media.mongodb.org/zips.json';
+$lines = file($filename, FILE_IGNORE_NEW_LINES);
 
-$dsn = 'mongodb://127.0.0.1:27017';
-// 链接服务器
-try {
-    $m = new MongoClient($dsn);
+$bulk = new MongoDB\Driver\BulkWrite;
 
-    print_r($m->listDBs());
-    // 选择一个数据库
-    $db = $m->selectDB('db1');
-
-    print_r($db->getCollectionInfo());
-
-    // 选择一个集合（ Mongo 的“集合”相当于关系型数据库的“表”）
-    $collection = $db->selectCollection('cartoons');
-
-//     // 插入一个文档（译注：“文档”相当于关系型数据库的“行”）
-//     $document = array( "title" => "Calvin and Hobbes", "author" => "Bill Watterson" );
-//     $collection->insert($document);
-
-//     // 添加另一个文档，它的结构与之前的不同
-//     $document = array( "title" => "机器猫", "online" => false, "contry" => "Japan" );
-//     $collection->insert($document);
-
-//     // 查询集合中的所有文档
-//     $cursor = $collection->find();
-
-//     // 遍历查询结果
-//     foreach ($cursor as $document) {
-//     var_dump($document);
-//     }
-} catch (MongoConnectionException $e) {
-    print_r($e->getMessage());
+foreach ($lines as $line) {
+    $bson = MongoDB\BSON\fromJSON($line);
+    $document = MongoDB\BSON\toPHP($bson);
+    $bulk->insert($document);
 }
+
+$manager = new MongoDB\Driver\Manager('mongodb://127.0.0.1/');
+
+$result = $manager->executeBulkWrite('test.zips', $bulk);
+printf("Inserted %d documents\n", $result->getInsertedCount());
--- src/redis/redis.php
+++ src/redis/redis.php
@@ -5,8 +5,9 @@ $redis = new Redis();
 try {
     $redis->connect('127.0.0.1', 6379);
 
-    $name = $redis->get('name');
-    var_dump($name);
+    for ($i=1;$i<=10000; $i++){
+        $redis->set('name:'.$i, $i, 3600);
+    }
 } catch (RedisException $e) {
     var_dump($e->getMessage());
 }
